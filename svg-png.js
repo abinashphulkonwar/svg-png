c48714a05ae77a677748c75c3b645b9553065d8d
abinash <abinashphulkonwar1@gmail.com>
2025-01-08 17:27:49 +0530

    add convert svg to png

---
+++ svg-png.js
@@ -0,0 +1,20 @@
+const { readFileSync, writeFileSync } = require("fs");
+const sharp = require("sharp");
+
+const ConvertSvgToPng = async (svgPath, outputPath) => {
+  const data = readFileSync(svgPath, "utf-8");
+
+  const buffer = Buffer.from(data);
+
+  await sharp(buffer)
+    .resize(800)
+    .png({ quality: 100 })
+    .toFile(outputPath) // Change 'output.jpg' for JPG
+    .then(() => {
+      console.log("Image successfully saved as PNG");
+    })
+    .catch((err) => {
+      console.error("Error processing the image:", err);
+    });
+};
+ConvertSvgToPng("./3.txt", "./3.png");
